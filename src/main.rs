2195dc9131e613aacf8e39bc3d8cdefc4d3e2407
Jose Marco Nufio Garrido <jose.m.nufio.0@gmail.com>
2024-11-28 18:26:36 +0100

    [FEAT] Show list of possible programs from os PATH variable user might want to run

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::io::{Write};
 use crossterm::{self, QueueableCommand, cursor, terminal, event};
 use crossterm::style::Stylize;
 use whoami::fallible as whoami;
+use is_executable::IsExecutable;
 
 fn main() -> process::ExitCode {
     let mut args = env::args();
@@ -56,6 +57,10 @@ fn run(program_name: &str, _args: env::Args) -> Result<(), String> {
     // Environment
     let mut stdout = io::stdout();
     let mut stderr = io::stderr();
+    let env_exes   = match query_env_exes() {
+        Ok(list) => list,
+        Err(_) => Vec::new(),
+    };
     
     // Setup base data
     let mut should_quit = false;
@@ -93,7 +98,14 @@ fn run(program_name: &str, _args: env::Args) -> Result<(), String> {
         stdout.uqueue(cursor::MoveDown(1))?;
         stdout.uqueue(cursor::MoveToColumn(0))?;
         // Activate raw mode temporarily to read the user input by hand a character at a time
-        let (line, close_requested) = handle_user_input(&mut stdout, &username, &git_branch_name, &mut cols, &mut rows)?;
+        let (line, close_requested) = handle_user_input(
+            &mut stdout,
+            &username,
+            &git_branch_name,
+            &mut cols,
+            &mut rows,
+            &env_exes.iter().map(|(_, name)| name.clone()).collect(),
+        )?;
         
         // Don't overlap with the design thingy
         stdout.ubwrite("\n")?;
@@ -188,6 +200,66 @@ fn run(program_name: &str, _args: env::Args) -> Result<(), String> {
             }
         }
 
+        if cfg!(debug_assertions) {
+            if uprog_name.to_lowercase() == "print-exes" {
+                stdout.ubwrite(format!("Known executable files({}):\n", env_exes.len()))?;
+                for (path, name) in env_exes.iter() {
+                    stdout.ubwrite(format!(" - {}\n   @ {}\n", name, path.display()))?;
+                }
+                stdout.ubwrite("\n")?;
+                continue;
+            }
+        }
+        if cfg!(debug_assertions) {
+            if uprog_name.to_lowercase() == "print-pp" {
+                for (k, v) in std::env::vars_os() {
+                    if k.to_string_lossy().to_string().to_lowercase() != "path" {
+                        continue;
+                    }
+                    for path_str in v.to_string_lossy().to_string().split(if cfg!(windows) { ';' } else { ':' }) {
+                        if path_str.is_empty() {
+                            continue;
+                        }
+                        let path = path::Path::new(path_str);
+                        stdout.ubwrite(format!("- {}\n", path_str))?;
+                        if ! path.exists() {
+                            stdout.ubwrite(" - Exists: false\n")?;
+                            continue;
+                        }
+                        stdout.ubwrite(" - Exists: true\n")?;
+                        if ! path.is_dir() {
+                            stdout.ubwrite(" - IsFolder: false\n")?;
+                            continue;
+                        }
+                        stdout.ubwrite(" - IsFolder: true\n")?;
+                        if let Ok(entries) = path.read_dir() {
+                            stdout.ubwrite(" - CanRead: true\n")?;
+                            for entry_res in entries {
+                                match entry_res {
+                                    Ok(entry) => {
+                                        stdout.ubwrite(format!(" - EntryPath: {}\n", entry.path().display()))?;
+                                        let entry_path = entry.path();
+                                        if !entry_path.is_file() {
+                                            stdout.ubwrite(" - Entry.IsFile: false\n")?;
+                                            continue;
+                                        }
+                                        stdout.ubwrite(" - Entry.IsFile: true\n")?;
+                                        stdout.ubwrite(format!(" - Entry.IsExe: {}\n", entry_path.is_executable()))?;
+                                    },
+                                    Err(err) => {
+                                        stdout.ubwrite(format!(" - EntryError: {}\n", err))?;
+                                    },
+                                };
+                            }
+                        } else {
+                            stdout.ubwrite(" - CanRead: false\n")?;
+                        }
+                    }
+                }
+                continue;
+            }
+        }
+
         if uprog_name.to_lowercase() == "echo" {
             let mut first = true;
             for uarg in req.start.get_args().map(|x| x.to_string_lossy()) {
@@ -240,12 +312,20 @@ fn run(program_name: &str, _args: env::Args) -> Result<(), String> {
 }
 
 
-fn handle_user_input(stdout: &mut io::Stdout, username: &str, git_branch_name: &str, cols: &mut u16, rows: &mut u16) -> Result<(String, bool), String> {
+fn handle_user_input(
+    stdout: &mut io::Stdout,
+    username: &str,
+    git_branch_name: &str,
+    cols: &mut u16,
+    rows: &mut u16,
+    env_exes: &Vec<String>
+) -> Result<(String, bool), String> {
     terminal::enable_raw_mode().iu()?;
+    
     let mut buf = String::new();
-    #[allow(unused_mut)]
     let mut sgs = Vec::new();
-    let draw_line = move |stdout: &mut io::Stdout, usr_txt: &str, _suggestions: &Vec<String>| -> Result<(), String> {
+    // TODO: Also include current directory stuff into suggestions
+    let draw_line = move |stdout: &mut io::Stdout, cols: u16, usr_txt: &str, suggestions: &Vec<&String>| -> Result<(), String> {
         stdout.uqueue(cursor::MoveToColumn(0))?;
         stdout.uqueue(terminal::Clear(terminal::ClearType::CurrentLine))?;
         stdout.uswrite("╠┈".cyan().on_black())?;
@@ -262,14 +342,52 @@ fn handle_user_input(stdout: &mut io::Stdout, username: &str, git_branch_name: &
         stdout.uqueue(cursor::MoveToColumn(0))?;
         stdout.uqueue(terminal::Clear(terminal::ClearType::CurrentLine))?;
         stdout.uswrite("╚═══════╝".cyan().on_black())?;
-        stdout.uswrite(" TODO: Implement auto complete options that should go here".dim().grey())?;
+        
+        if usr_txt.len() < 2 || suggestions.is_empty() {
+            stdout.uswrite(" {}".dim().grey())?;
+        } else {
+            let (x, _) = cursor::position().iu()?;
+            let mut it = suggestions.iter();
+            if let Some(first) = it.next() {
+                let mut x = x;
+                if !usr_txt.starts_with(&**first) {
+                    let s = format!(" {{{}", usr_txt);
+                    x += s.chars().count() as u16;
+                    stdout.uswrite(s.yellow())?;
+                    stdout.ubwrite("|")?;
+                    x += 1;
+                    let s = first.chars().skip(usr_txt.len()).collect::<String>();
+                    x += s.chars().count() as u16 + 1u16;
+                    stdout.ubwrite(format!("{}}}", s))?;
+                } else {
+                    stdout.uswrite(" {".dim())?;
+                    x += 2;
+                    stdout.uswrite(format!("{}", first).yellow())?;
+                    x += first.chars().count() as u16;
+                    stdout.uswrite("}".dim())?;
+                    x += 2;
+                }
+                while let Some(item) = it.next() {
+                    let s = format!(" | {}", item);
+                    x += s.chars().count() as u16;
+                    if x >= cols {
+                        break;
+                    }
+                    stdout.uswrite(s.dim())?;
+                }
+            }
+        }
         stdout.uqueue(cursor::RestorePosition)?;
         stdout.uflush()?;
         Ok(())
     };
 
-    draw_line(stdout, &buf, &sgs)?;
+    for e in env_exes.iter() {
+        sgs.push(e.clone());
+    }
+    draw_line(stdout, *cols, &buf, &sgs.iter().collect())?;
     let mut is_done = false;
+    let mut last_suggestion = None;
     while !is_done {
         if event::poll(time::Duration::ZERO).iu()? {
             match event::read().iu()? {
@@ -284,18 +402,43 @@ fn handle_user_input(stdout: &mut io::Stdout, username: &str, git_branch_name: &
                     if event.kind != event::KeyEventKind::Press {
                         break 'key_event_block;
                     }
-                    // let capitalize = false;
                     if !event.modifiers.is_empty() {
+                        if event.modifiers == event::KeyModifiers::CONTROL {
+                            if event.code == event::KeyCode::Backspace {
+                                // Remove all whitespace ahead of characters cause I think that is what feels natural
+                                if let Some(mut c) = buf.pop() {
+                                    while c.is_whitespace() {
+                                        if let Some(nx) = buf.pop() {
+                                            c = nx;
+                                        } else {
+                                            break;
+                                        }
+                                    }
+                                }
+                                // This should stop when hitting whitespace and symbols
+                                while let Some(ch) = buf.pop() {
+                                    if !ch.is_alphanumeric() {
+                                        break;
+                                    }
+                                }
+                            }
+                            break 'key_event_block;
+                        }
                         if event.modifiers != event::KeyModifiers::SHIFT {
                             break 'key_event_block;
                         }
-                        // captialize = true;
                     }
                     match event.code {
-                        // event::KeyCode::Char(c) => { buf.push(if capitalize { c.to_uppercase() } else { c }); },
                         event::KeyCode::Char(c) => { buf.push(c); },
                         event::KeyCode::Backspace => { let _ = buf.pop(); },
                         event::KeyCode::Enter => { is_done = true; },
+                        event::KeyCode::Tab => {
+                            if let Some(sg) = last_suggestion {
+                                buf.clear();
+                                buf.push_str(sg);
+                                buf.push(' ');
+                            }
+                        },
                         _ => {},
                     };
                 },
@@ -304,8 +447,11 @@ fn handle_user_input(stdout: &mut io::Stdout, username: &str, git_branch_name: &
                 _ => {},
             };
         }
-        draw_line(stdout, &buf, &sgs)?;
+        let sgs:Vec<_> = sgs.iter().filter(|name| name.starts_with(&buf)).collect();
+        last_suggestion = sgs.first().map(|x| x.as_str());
+        draw_line(stdout, *cols, &buf, &sgs)?;
     }
+    draw_line(stdout, *cols, &buf, &Vec::new())?;
     stdout.uqueue(cursor::MoveDown(1))?;
     terminal::disable_raw_mode().iu()?;
     return Ok((buf, false));
@@ -506,6 +652,130 @@ fn resolve_symlink(path: &path::Path) -> Result<(path::PathBuf, Vec<String>), St
 }
 
 
+fn query_env_exes() -> io::Result<Vec<(path::PathBuf, String)>> {
+    let mut exes = Vec::new();
+    for (key, val) in std::env::vars_os() {
+        // Ignore anything that's not in the PATH variable
+        if key.to_string_lossy().to_string().to_lowercase() != "path" {
+            continue;
+        }
+        
+        let mut paths = Vec::new();
+        let val_string = val.to_string_lossy().to_string();
+        #[cfg(target_family="windows")]
+        const ENV_DELIM:char = ';';
+        #[cfg(target_family="unix")]
+        const ENV_DELIM:char = ':';
+        
+        if val_string.contains(ENV_DELIM) {
+            for path in std::env::split_paths(&val) {
+                if !path.exists() {
+                    continue;
+                }
+                if path.is_file() {
+                    paths.push(path);
+                    continue;
+                }
+                if !path.is_dir() {
+                    continue;
+                }
+                if let Ok(entries) = path.read_dir() {
+                    for entry_res in entries {
+                        if let Ok(entry) = entry_res {
+                            let entry_path = entry.path();
+                            if !entry_path.is_file() {
+                                continue;
+                            }
+                            paths.push(entry_path);
+                        }
+                    }
+                }
+            }
+        } else {
+            let path = path::PathBuf::from(&val);
+            let exists = path.exists();
+            if !exists {
+            } else if path.is_file() {
+                paths.push(path);
+            } else if path.is_dir() {
+                if let Ok(entries) = path.read_dir() {
+                    for entry_res in entries {
+                        if !entry_res.is_ok() {
+                            continue;
+                        }
+                        let entry_path = entry_res.unwrap().path();
+                        if !entry_path.is_file() {
+                            continue;
+                        }
+                        paths.push(entry_path);
+                    }
+                }
+            }
+        }
+        let paths = paths;
+        
+        for path in paths.iter() {
+            // TODO: Inspect symlinks I guess
+            if path.is_symlink() {
+                continue;
+            }
+            #[cfg(target_family="windows")]
+            if path.is_file() {
+                if let Some(ext) = path.extension() {
+                    // If a file don't end with .bat or .exe, it's not worth even checking if it's executable
+                    // If you don't know why, the reason is "windows" trust me
+                    if ext != "bat" && ext != "exe" {
+                        continue;
+                    }
+                }
+            }
+
+            if !path.is_executable() {
+                continue;
+            }
+            
+            match path.file_name() {
+                None => {},
+                Some(os_name) => match os_name.to_str() {
+                    None => {},
+                    Some(name) => {
+                        let name = name.to_string();
+                        // To be honest, I'm not sure which one window's picks as priority .bat or .exe
+                        // so best not to show the file extensions. Also if you have done this, that's
+                        // kinda cringe bro. (I've done this, I need to clean my path variable)
+                        #[cfg(target_family="windows")]
+                        let name = match name.strip_suffix(".exe") {
+                            None =>  name,
+                            Some(stripped_name) => stripped_name.to_string(),
+                        };
+                        #[cfg(target_family="windows")]
+                        let name = match name.strip_suffix(".bat") {
+                            None =>  name,
+                            Some(stripped_name) => stripped_name.to_string(),
+                        };
+                        exes.push((path.to_owned(), name));
+                    },
+                },
+            };
+        }
+    }
+
+    let mut names_added = Vec::new();
+    let exes:Vec<_> = exes.into_iter().filter(|(_, name)| {
+        if names_added.contains(name) {
+            false
+        } else {
+            names_added.push(name.to_string());
+            true
+        }
+    }).collect();
+    
+    return Ok(exes);
+}
+
+
+
+
 
 // TODO: Is it better to do a search for the .git folder? Did it this way cause it was the easiest and "it just works" - Tod Howard
 fn query_git_branch_name() -> io::Result<Option<String>> {
@@ -525,10 +795,12 @@ fn query_git_branch_name() -> io::Result<Option<String>> {
         });
 }
 
+
 // Not sure if to keep whoami so for now it's placed in this little isolation box we call a function.
 fn query_username() -> Result<String, io::Error> {
     return whoami::username();
 }
+
 
 fn query_current_directory_name() -> Result<(path::PathBuf, String), String> {
     let dir = env::current_dir().iu()?;
@@ -538,7 +810,6 @@ fn query_current_directory_name() -> Result<(path::PathBuf, String), String> {
     };
     return Ok((dir, file_name));
 }
-
 
 
 
